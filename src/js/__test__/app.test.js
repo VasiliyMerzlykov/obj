89a3643c8e62b2777c0ffc4b9c17fd6eba853c16
Vasiliy Merzlyakov <V.Merov@yandex.ru>
2020-11-04 17:02:21 +0500

    third commit

---
+++ src/js/__test__/app.test.js
@@ -0,0 +1,61 @@
+import { orderByProps, showAttacks } from '../app';
+
+test('need to display object properties', () => {
+  const player = {
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40,
+  };
+  const result = [
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+  ];
+
+  expect(orderByProps(player, ['name', 'level'])).toEqual(result);
+});
+
+test('special attacks', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  };
+
+  const result = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
+
+  expect(showAttacks(character)).toEqual(result);
+});
